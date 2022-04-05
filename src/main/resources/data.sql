f3770175b5c1c3f96e110c63eaf400ca85dc9bd3
ulsin <ulriksin@gmail.com>
2022-04-05 19:08:00 +0200

    📦 NEW: added one row in registration table for debug

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,4 +6,9 @@ values  ('Volvo','V30'),
         ('Audi','Q7'),
         ('Audi','Q8'),
         ('Toyota','Trueno'),
+        ('Toyota','Corolla'),
         ('Toyota','Levin');
+
+-- demo to debug button
+insert into Registration(ssn, name, address, characteristics, brand, type)
+values ('12345678901', 'Ulrik', 'OsloMet', 'AB12345', 'Toyota', 'Corolla');
